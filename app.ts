93d387c2f6c78a2c596d57b6fc8c86001a95c75b
Ceeessan <Cessan__94@hotmail.com>
2024-08-20 17:21:23 +0200

    Update json

--- app.ts
+++ app.ts
@@ -96,28 +96,6 @@ function validate(validatableInput: Validatable){
     return isValid;
 }
 
-//autobind decorator || används ej.
-/*
-function autobind(
-    target: any, 
-    methodName: string, 
-    descriptor: PropertyDescriptor
-): PropertyDescriptor {
-    const originalMethod = descriptor.value;
-
-    const adjDescriptor: PropertyDescriptor = {
-        configurable: true,
-        enumerable: false,
-        get() {
-            const boundFn = originalMethod.bind(this);
-            return boundFn;
-        }
-    };
-
-    return adjDescriptor;
-}
-    */
-
 //Component Base Class
 abstract class Component <T extends HTMLElement, U extends HTMLElement> {
     templateElement : HTMLTemplateElement;
